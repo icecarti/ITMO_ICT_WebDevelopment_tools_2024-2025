cdf948ff8b63fe92aedf7d859ba7d338c8ecefa8
icecarti <and08sha@gmail.com>
2025-04-13 22:39:57 +0300

    done practice 2

--- laboratory_work/main.py
+++ laboratory_work/main.py
@@ -1,65 +1,93 @@
-from fastapi import FastAPI
-from typing import List
+from fastapi import FastAPI, Depends, HTTPException
+from contextlib import asynccontextmanager
+from sqlmodel import select
 from typing_extensions import TypedDict
-from models import Profile
-from mock import mock_db
 
-app = FastAPI()
-
-
-class CreateResponse(TypedDict):
-    status: int
-    data: Profile
+from connection import init_db, get_session
+from models.models import *
+from models.public_models import ProfilePublic, BookPublic
 
 
-class UpdateResponse(TypedDict):
-    status: int
-    data: Profile
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    yield
 
 
-class DeleteResponse(TypedDict):
-    status: int
-    message: str
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Profile API"}
+    return {"message": "API is running"}
 
 
-@app.get("/profile_list", response_model=List[Profile])
-def profile_list():
-    return mock_db
+@app.get("/profile_list", response_model=list[ProfilePublic])
+def get_profile_list(session=Depends(get_session)):
+    return session.exec(select(Profile)).all()
 
 
-@app.get("/profile/{profile_id}", response_model=Profile)
-def profile_get(profile_id: int):
-    for profile in mock_db:
-        if profile["id"] == profile_id:
-            return profile
-    return {"message": "Profile not found"}
+@app.get("/profile/{profile_id}", response_model=ProfilePublic)
+def get_profile(profile_id: int, session=Depends(get_session)):
+    profile = session.get(Profile, profile_id)
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    return profile
 
 
-@app.post("/profile", response_model=CreateResponse)
-def profile_create(profile: Profile):
-    new_profile = profile.model_dump()
-    mock_db.append(new_profile)
-    return {"status": 201, "data": profile}
+@app.post("/profile")
+def create_profile(profile: ProfileDefault, session=Depends(get_session)) -> TypedDict("Response", {"status": int, "created": Profile}):
+    profile = Profile.model_validate(profile)
+    session.add(profile)
+    session.commit()
+    session.refresh(profile)
+    return {"status": 201, "created": profile}
 
 
-@app.put("/profile/{profile_id}", response_model=UpdateResponse)
-def profile_update(profile_id: int, updated_profile: Profile):
-    for i, profile in enumerate(mock_db):
-        if profile["id"] == profile_id:
-            mock_db[i] = updated_profile.model_dump()
-            return {"status": 205, "data": updated_profile}
-    return {"status": 404, "data": updated_profile}
+@app.patch("/profile/{profile_id}")
+def update_profile(profile_id: int, upd_profile: ProfileDefault, session=Depends(get_session)) -> TypedDict("Response", {"status": int, "updated": Profile}):
+    profile = session.get(Profile, profile_id)
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    upd_data = upd_profile.model_dump(exclude_unset=True)
+    for key, value in upd_data.items():
+        setattr(profile, key, value)
+    session.commit()
+    session.refresh(profile)
+    return {"status": 200, "updated": profile}
 
 
-@app.delete("/profile/{profile_id}", response_model=DeleteResponse)
-def profile_delete(profile_id: int):
-    for i, profile in enumerate(mock_db):
-        if profile["id"] == profile_id:
-            mock_db.pop(i)
-            return {"status": 202, "message": "deleted"}
-    return {"status": 404, "message": "Profile not found"}
+@app.delete("/profile/{profile_id}")
+def delete_profile(profile_id: int, session=Depends(get_session)) -> TypedDict("Response", {"status": int, "msg": str}):
+    profile = session.get(Profile, profile_id)
+    if not profile:
+        raise HTTPException(status_code=404, detail="Profile not found")
+    session.delete(profile)
+    session.commit()
+    return {"status": 204, "msg": "Profile deleted"}
+
+
+@app.get("/book_instance/{book_id}", response_model=BookPublic)
+def get_book_instance(book_id: int, session=Depends(get_session)):
+    book = session.get(Book, book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
+
+
+@app.post("/book_instance")
+def create_book_instance(new_book: BookDefault, session=Depends(get_session)) -> TypedDict("Response", {"status": int, "created": Book}):
+    new_book = Book.model_validate(new_book)
+    session.add(new_book)
+    session.commit()
+    session.refresh(new_book)
+    return {"status": 201, "created": new_book}
+
+
+@app.post("/book_info")
+def create_book_info(book: BookInfoDefault, session=Depends(get_session)) -> TypedDict("Response", {"status": int, "created": BookInfo}):
+    book = BookInfo.model_validate(book)
+    session.add(book)
+    session.commit()
+    session.refresh(book)
+    return {"status": 201, "created": book}
---
+++ laboratory_work/models/public_models.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from models.default_models import ProfileDefault, BookDefault, ShareRequestDefault
+
+class ProfilePublic(ProfileDefault):
+    id: int
+    books: Optional[list[BookDefault]] = []
+    sent_requests: Optional[list[ShareRequestDefault]] = []
+    received_requests: Optional[list[ShareRequestDefault]] = []
+
+
+class BookPublic(BookDefault):
+    id: int
+    owner: Optional[ProfileDefault] = None
